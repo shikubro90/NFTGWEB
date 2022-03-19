9957a2a78bcadc0fdabbf98b49c4423df8ade482
shikubro90 <shikubro90@gmail.com>
2022-03-19 18:26:15 +0600

    new Data

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container'
+import React from "react";
+import Home from "./pages/Home";
+
+function App() {
+
+  return (
+    <div className="App">
+     <Container>
+        <Home/>
+     </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        Hello React
+    </>
+  )
+}
+
+export default Home
