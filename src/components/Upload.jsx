d88006f4d66441364a667ad63b09b7c52bbf83f5
shikubro90 <shikubro90@gmail.com>
2022-04-02 15:09:39 +0600

    Day-7

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,15 +1,28 @@
 import { Box, Button } from '@mui/material'
 import React from 'react'
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+// import path from 'path';
+
+//gets your app's root path
+// const  root = path.dirname(require.main.filename)
+
 
 const Upload = () => {
 
     const dirHandeler= (e)=>{
         e.preventDefault();
-        let value = URL.createObjectURL(e.target.files[0])
+        const reader = new FileReader();
         // console.log(e.target.files[0])
-        console.log(value)
-
+        const fileList = Object.values(e.target.files);
+        // console.log(fileList)
+        fileList.map( async (file)=>{
+                reader.onload = (e) => {
+            //     // await localStorage.setItem(file.name, e.target.result);
+                console.log(e.target.result)
+            }
+            reader.readAsDataURL(file);
+                // console.log(file)
+        })
     }
 
   return (
